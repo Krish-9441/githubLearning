c19ee482792e7a66ab3b8ca21ca250e64300eee7
Krish_Kanjani <krishkanjani86@gmail.com>
2025-12-18 19:27:18 +0530

    Revert something

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ function sayName(){
     console.log(`My name is ${name}`);
 }
 
+<<<<<<< HEAD
 function newFn(){
     console.log('Added a new funtion ')
+=======
+function isAbove18(){
+    return age >=18
+>>>>>>> parent of 10ba330 (Removed 18 function)
 }
